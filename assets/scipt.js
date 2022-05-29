939f8d76a22522114ec3230a07165061a40d4a5c
Tazaev <tonymurtazaev@gmail.com>
2022-05-29 19:56:56 -0400

    added local sotrage portion

--- assets/scipt.js
+++ assets/scipt.js
@@ -38,6 +38,16 @@ $(".time-block").each(function () {
     $(this).removeClass("past");
     $(this).addClass("future");
   }
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 })
 
 // fetch data from local storage portion here
