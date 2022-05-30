fbba108ecda758c8f6f40e55bf6113370ce637e4
Tazaev <tonymurtazaev@gmail.com>
2022-05-30 13:15:20 -0400

    dubugging cont.

--- assets/scipt.js
+++ assets/scipt.js
@@ -6,7 +6,7 @@ $("#currentDay").html(currenttime);
 //event listener button for save
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
-    var text = $(this).sibling(".description").val();
+    var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     //store saved data in local storage
@@ -14,7 +14,7 @@ $(document).ready(function () {
   });
 
   // function to get the current time
-  function time() {
+  function timetrack() {
     var timenow = moment().hour();
 
     // function to loop through timeblocks
@@ -23,14 +23,14 @@ $(document).ready(function () {
 
       // function for added content to time blocks
       if (blocktime < timenow) {
-        $(this).removeclass("future");
-        $(this).removeclass("present");
-        $(this).addclass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
         // had to add "if" to make this work
       } else if (blocktime === timenow) {
-        $(this).removeclass("past");
-        $(this).removeclass("future");
-        $(this).addclass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
       } else {
         $(this).removeClass("present");
         $(this).removeClass("past");
@@ -48,7 +48,7 @@ $(document).ready(function () {
   $("#hour15 .description").val(localStorage.getItem("hour15"));
   $("#hour16 .description").val(localStorage.getItem("hour16"));
   $("#hour17 .description").val(localStorage.getItem("hour17"));
-  time();
+  timetrack();
 });
 
 // fetch data from local storage portion here
